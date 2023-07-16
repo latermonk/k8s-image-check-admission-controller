cfc6e3b214a4318a4a3f4c79eca22582b30ef29f
latemonk <iback.china@gmail.com>
2023-07-16 15:40:19 +0800

    init with code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # k8s-image-check-admission-controller
 k8s-image-check-admission-controller
+
+
+```shell
+docker  pull  nginx
+```
+
+
+```shell
+go mod init k8s-image-check-admission-controller
+go mod tidy && go run main.go
+```
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.41"))
+	if err != nil {
+		panic(err)
+	}
+
+	// get image information
+	imageName := "nginx:latest"
+	args := filters.NewArgs(filters.Arg("reference", imageName))
+	images, err := cli.ImageList(ctx, types.ImageListOptions{Filters: args})
+	if err != nil {
+		panic(err)
+	}
+
+	if len(images) != 1 {
+		panic(fmt.Errorf("Can't find image %s", imageName))
+	}
+
+	// get image size，shown in MB
+	imageSize := float32(images[0].Size) / (1024 * 1024)
+	fmt.Printf("IMage %s size =  %.2f MB\n", imageName, imageSize)
+}
